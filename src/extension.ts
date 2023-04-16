45ef3be44e8dd2fac7faf960156912f2fe46a0f6
Fabian Jakobs <fabian.jakobs@web.de>
2023-04-16 16:49:00 +0200

    Prepare for release

--- src/extension.ts
+++ src/extension.ts
@@ -2,18 +2,9 @@ import * as vscode from "vscode";
 import { activateLanguageClient, deactivateLanguageClient } from "./lsp-client";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "jslox" is now active!');
-
-    let disposable = vscode.commands.registerCommand("jslox.helloWorld", () => {
-        vscode.window.showInformationMessage("Hello World from jslox!");
-    });
-
-    context.subscriptions.push(disposable);
-
     activateLanguageClient(context);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {
     deactivateLanguageClient();
 }
